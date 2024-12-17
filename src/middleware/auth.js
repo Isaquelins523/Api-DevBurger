4ad5c01306dd1b263aa3ac1f6900c523bc94256e
Isaque Lins <isaquelins523@gmail.com>
2024-12-17 19:26:12 -0300

    Alterção no middleware

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,30 +1,35 @@
 import jwt from 'jsonwebtoken'
 import authConfig from '../config/auth'
 
+export default function authMiddleware(request, response, next) {
 
-function authMiddleware(request, response, next) {
-    const authToken = request.headers.authorization
+    const authToken = request.headers.authorization;
 
     if (!authToken) {
-        return response.status(401).json({ error: 'Token not provided' })
+
+        return response.status(401).json({ error: 'Token not provid' })
     }
 
     const token = authToken.split(' ').at(1)
+    // const [_,token] = authToken.split(' ')[1]
 
     try {
+
         jwt.verify(token, authConfig.secret, (err, decoded) => {
+
             if (err) {
-                throw new Error()
+                throw new Error();
             }
-            request.userId = decoded.id 
-            
+
+            request.userId = decoded.id
+            request.userName = decoded.name
+
         })
-    } catch (error) {
+
+    } catch (err) {
+        console.error(err)
         return response.status(401).json({ error: 'Token is invalid' })
     }
 
-    return next()
+    return next();
 }
-
-
-export default authMiddleware
